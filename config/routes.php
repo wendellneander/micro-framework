b2a140ae3b2bb42d9881b13e7b4174d177544d91
Wendell Neander <neanderdev@gmail.com>
2019-04-12 12:59:54 -0300

    Add routes

--- config/routes.php
+++ config/routes.php
@@ -2,4 +2,9 @@
 
 $router = \Core\Router::getInstance();
 
-$router->route('/', 'WelcomeController@index');
+$router->route('/', 'StoreController@index');
+$router->route('/new', 'StoreController@create');
+$router->route('/edit/{id}', 'StoreController@edit');
+$router->route('/save/{id}', 'StoreController@save');
+$router->route('/update/{id}', 'StoreController@update');
+$router->route('/delete/{id}', 'StoreController@delete');
