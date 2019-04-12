31f5cd38ec8d527faf5df5cc2cfa6be1450c44b1
Wendell Neander <neanderdev@gmail.com>
2019-04-12 13:18:52 -0300

    :lipstick: Buildinh views

--- views/store/index.blade.php
+++ views/store/index.blade.php
@@ -1,4 +1,18 @@
 @extends('template')
 
 @section('content')
+    <ul class="list-group">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            Cras justo odio
+            <span class="btn btn-outline-primary">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            Dapibus ac facilisis in
+            <span class="btn btn-outline-primary">2</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            Morbi leo risus
+            <span class="btn btn-outline-primary">1</span>
+        </li>
+    </ul>
 @endsection
