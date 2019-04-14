b52108e168a540b00c583853a922ebdc8fdf6551
Wendell Neander <neanderdev@gmail.com>
2019-04-13 22:43:33 -0300

    Database connection

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 $database = \Core\DataBase::getInstance();
 
-$database->addConfig('local', [
+$database->addConfig('db', [
     'driver' => getenv('DB_DRIVER'),
     'host' => getenv('DB_HOST'),
     'database' => getenv('DB_DATABASE'),
@@ -12,14 +12,4 @@ $database->addConfig('local', [
     'collation' => getenv('DB_COLLATION')
 ]);
 
-$database->addConfig('production', [
-    'driver' => getenv('DB_DRIVER'),
-    'host' => getenv('DB_HOST'),
-    'database' => getenv('DB_DATABASE'),
-    'username' => getenv('DB_USER'),
-    'password' => getenv('DB_PASSWORD'),
-    'charset' => getenv('DB_CHARSET'),
-    'collation' => getenv('DB_COLLATION')
-]);
-
-$database->setCurrentConfig('local');
+$database->setCurrentConfig('db');
