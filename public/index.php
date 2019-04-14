78708a5a771d73f3f3fcf40bfecca645bcb96e1e
Wendell Neander <neanderdev@gmail.com>
2019-04-13 23:01:45 -0300

    test

--- public/index.php
+++ public/index.php
@@ -6,13 +6,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 try{
     \Dotenv\Dotenv::create(__DIR__ . '/..')->load();
-}catch (Exception $e){}
 
-require_once __DIR__ . '/../config/routes.php';
+    require_once __DIR__ . '/../config/routes.php';
 
-require_once __DIR__ . '/../config/database.php';
+    require_once __DIR__ . '/../config/database.php';
 
-$bootstrap = \Core\Bootstrap::getInstance();
+    $bootstrap = \Core\Bootstrap::getInstance();
 
-$bootstrap->start();
-
+    $bootstrap->start();
+}catch (Exception $e){
+    var_dump($e);
+    exit;
+}
