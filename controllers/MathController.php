01338a7b85566f1f738987cab22dba60ab96147b
Wendell Neander <neanderdev@gmail.com>
2019-04-13 21:36:11 -0300

    Validator

--- controllers/MathController.php
+++ controllers/MathController.php
@@ -3,6 +3,9 @@
 namespace Controllers;
 
 use Core\Controller;
+use Core\Request;
+use Core\Session;
+use Core\Validator;
 
 class MathController extends Controller
 {
@@ -11,33 +14,83 @@ class MathController extends Controller
 
     public function calculateScore($score1, $score2, $score3, $score4)
     {
-        $score = ($score1 + $score2 + $score3 + $score4) / 4;
+        try {
+            Validator::getInstance()->validate([
+                'score1' => $score1,
+                'score2' => $score2,
+                'score3' => $score3,
+                'score4' => $score4,
+            ],[
+                'score1' => 'integer',
+                'score2' => 'integer',
+                'score3' => 'integer',
+                'score4' => 'integer',
+            ]);
 
-        $message = 'Aluno Reprovado';
+            $score = ($score1 + $score2 + $score3 + $score4) / 4;
 
-        if ($score >= 7) {
-            $message = 'Aluno Aprovado';
+            $message = 'Aluno Reprovado';
+
+            if ($score >= 7) {
+                $message = 'Aluno Aprovado';
+            }
+
+            $this->view('math/score', ['score' => $score, 'message' => $message]);
+        } catch (\Exception $exception) {
+            Session::flash('message', $exception->getMessage());
+
+            Request::redirect('/');
         }
-
-        $this->view('math/score', ['score' => $score, 'message' => $message]);
     }
 
     public function calculateTemperature($celsius)
     {
-        $fahrenheit = ($celsius * 9 / 5) + 32;
+        try {
+            Validator::getInstance()->validate([
+                'celsius' => $celsius
+            ],[
+                'celsius' => 'integer'
+            ]);
 
-        $this->view('math/temperature', ['celsius' => $celsius, 'fahrenheit' => $fahrenheit]);
+            $fahrenheit = ($celsius * 9 / 5) + 32;
+
+            $this->view('math/temperature', ['celsius' => $celsius, 'fahrenheit' => $fahrenheit]);
+        } catch (\Exception $exception) {
+            Session::flash('message', $exception->getMessage());
+
+            Request::redirect('/');
+        }
+
     }
 
     public function calculateAge($years, $months, $days)
     {
-        $ageInDays = $years * self::DAYS_IN_A_YEAR;
 
-        $ageInDays += $months * self::DAYS_IN_A_MONTH;
+        try {
+            Validator::getInstance()->validate([
+                'years' => $years,
+                'months' => $months,
+                'days' => $days,
+            ],[
+                'years' => 'integer',
+                'months' => 'integer',
+                'days' => 'integer',
+            ]);
 
-        $ageInDays += $days;
 
-        $this->view('math/age', ['age' => $ageInDays]);
+            $ageInDays = $years * self::DAYS_IN_A_YEAR;
+
+            $ageInDays += $months * self::DAYS_IN_A_MONTH;
+
+            $ageInDays += $days;
+
+            $this->view('math/age', ['age' => $ageInDays]);
+        } catch (\Exception $exception) {
+            Session::flash('message', $exception->getMessage());
+
+            Request::redirect('/');
+        }
+
     }
 
 }
---
+++ core/Validator.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Core;
+
+
+class Validator
+{
+    const RULE_INTEGER = 'integer';
+    const RULE_NUMERIC = 'numeric';
+    const RULE_STRING = 'string';
+    const RULE_BOOLEAN = 'boolean';
+
+    /**
+     * @var self $instance
+     */
+    private static $instance;
+
+    private $rules = [
+        self::RULE_INTEGER,
+        self::RULE_NUMERIC,
+        self::RULE_STRING,
+        self::RULE_BOOLEAN
+    ];
+
+    /**
+     * @return Validator
+     */
+    public static function getInstance()
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * @param $data
+     * @param $rules
+     * @throws \Exception
+     */
+    public function validate(array $data, array $rules)
+    {
+        $this->checkRules($rules);
+
+        foreach ($data as $attribute => $value) {
+            if (!array_key_exists($attribute, $rules)) {
+                continue;
+            }
+
+            $rules = explode('|', $rules[$attribute]);
+
+            foreach ($rules as $rule) {
+                $this->applyRule($rule, $attribute, $value);
+            }
+        }
+    }
+
+    /**
+     * @param array $rules
+     * @throws \Exception
+     */
+    private function checkRules(array $rules)
+    {
+        foreach ($rules as $field => $rule) {
+
+            $rulesArray = explode('|', $rule);
+
+            foreach ($rulesArray as $r) {
+                if (!in_array($r, $this->rules)) {
+                    throw new \Exception('Rule ' . $r . ' does not exists');
+                }
+            }
+        }
+    }
+
+    /**
+     * @param $rule
+     * @param $attribute
+     * @param $value
+     * @throws \Exception
+     */
+    private function applyRule($rule, $attribute, $value)
+    {
+        switch ($rule) {
+            case self::RULE_BOOLEAN :
+                if(!is_bool($value)){
+                    throw new \Exception($attribute . ' must be a boolean');
+                }
+            case self::RULE_INTEGER :
+                if(!is_integer($value)){
+                    throw new \Exception($attribute . ' must be a integer');
+                }
+            case self::RULE_NUMERIC :
+                if(!is_numeric($value)){
+                    throw new \Exception($attribute . ' must be a numeric');
+                }
+            case self::RULE_STRING :
+                if(!is_string($value)){
+                    throw new \Exception($attribute . ' must be a string');
+                }
+        }
+    }
+}
